18e4df7bc305e95cec7bda40a0cff967c6fe54c7
Nasicas <https://Nasicas7@github.com>
2022-10-26 18:29:09 +0200

    premier commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+import HomeViewVue from './views/HomeView.vue';
+
+
+let string= "Rob";
+
+</script>
+
+<template>
+
+bonjour
+{{string}}  
+
+<div class="container"></div>
+<form action="">
+  <input type="text" placeholder="search" name="inputext" id="">
+  <button type="submit" class="button_search"></button>
+</form>
+
+</template>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container{
+  width: 100%;
+  min-height: 100hp;
+  padding: 5%;
+}
+
+.button_search{
+  width: 10hp;
+  height: 10 hp;
+  padding: 5%;
+}
+
+
+</style>
