38339bfd4076d286f882adf87fdcc769d85fa6d5
rpalmier <rpalmier@et.intechinfo.fr>
2022-11-22 17:42:02 +0100

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
 
+import { ref } from 'vue';
 import HomeViewVue from './views/HomeView.vue';
+import AboutViewVue from './views/AboutView.vue';
+import ContactViewVue from './views/ContactView.vue';
 
 
-let string= "Rob";
+
+const home = ref(true);
+const contact = ref(false);
+const about = ref(false);
+
+
+function displayHome() {
+  home.value = true
+  contact.value = false
+  about.value = false
+}
+
+function displayContact() {
+  home.value = false
+  contact.value = true
+  about.value = false
+  /*contact.value = !contact.value*/
+}
+
+function displayAbout() {
+  home.value = false
+  contact.value = false
+  about.value = true
+  /* about.value = !about.value*/
+}
+
+function fetchTest() {
+  fetch('http://localhost:8080/messages')
+    .then(response => console.log(response.text()))
+}
 
 </script>
 
@@ -12,28 +44,20 @@ let string= "Rob";
   <!-- Menu de navigation-->
   <header>
     <div class="topnav">
-      <a class="active" href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#contact">Contact</a>
+      <button class="btnMenu" @click="displayHome">Home</button> <!-- <a class="active" href="#home">Home</a>-->
+      <button class="btnMenu" @click="displayContact">Contact</button> <!-- <a href="#contact">Contact</a>-->
+      <button class="btnMenu" @click="displayAbout">About</button> <!-- <a href="#about">About</a> -->
+      <button class="btnMenu" @click="fetchTest">fetchTest</button>
     </div>
   </header>
 
+ 
+
+  <HomeViewVue v-if="home" />
+  <ContactViewVue v-if="contact" />
+  <AboutViewVue v-if="about" />
 
   <div class="separateur"></div>
-
-  <div class="sBar">
-    <input type="search" class="searchBar" placeholder="Search...">
-  </div>
-
-  <div class="separateur"></div>
-
-  <div id="content">
-    <div> <img src="bg.png"> </div>
-    <div id="autre"> <img src="bg.png"> </div>
-    <div class="separateur"></div>
-
-  </div>
-
 
   <!-- Pied de page -->
   <footer>
@@ -41,21 +65,27 @@ let string= "Rob";
       Copyright &copy; In'dev 2022-2023 - All right Reserved
     </p>
   </footer>
-
 </template>
 
 <style>
-
-* {box-sizing: border-box;}
+* {
+  box-sizing: border-box;
+  /*align-items: center;*/
+}
 
 body {
-margin: 0;
-font-family: Arial, Helvetica, sans-serif;
-
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
 }
 
 header {
-  background-color:#282A35;
+  background-color: #282A35;
+
+}
+
+.btnMenu {
+  text-align: center;
+  padding: 1em;
 }
 
 div#content div {
@@ -64,74 +94,137 @@ div#content div {
   vertical-align: top;
   margin: 10px;
 }
-div#content{
+
+div#content {
   background-color: #E8E8E8;
 }
 
+.home {
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+.wall {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 90%;
+  height: 30em;
+  background-color: #292b2f;
+  justify-content: space-between;
+  margin-top: 5%;
+  border-radius: 15px;
+}
+
+.cards {
+  display: flex;
+  background-color: #36393f;
+  width: 22em;
+  height: 9em;
+  margin: 0.5em;
+  border-radius: 10px;
+}
+
 .sBar {
-display: flex; 
-justify-content: center; 
-align-items: center;
-min-height: 45%;
-vertical-align: top;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 45%;
+  vertical-align: top;
+
 }
 
 
 .searchBar {
-width: 40%;
-height: 10%;
-border: 1px black solid;
-border-radius: 25px;
+  width: 40%;
+  height: 10%;
+  border: 1px black solid;
+  border-radius: 25px;
+  margin-top: 0.25rem;
+  border-width: 2px;
+  border-radius: 9999px;
+  border-style: solid;
 
 }
 
-.btnSearch{
-padding-left: 5%;
+.btnSearch {
+  padding-left: 5%;
 }
 
 
 .topnav {
-overflow: hidden;
-background-color: #e9e9e9;
+  overflow: hidden;
+  background-color: cadetblue;
+  padding-bottom: 0.5%;
+  padding-top: 0.5%;
+  padding-left: 1%;
+  padding-right: 1%;
 }
 
 .topnav a {
-float: left;
-display: block;
-color: black;
-text-align: center;
-padding: 14px 16px;
-text-decoration: none;
-font-size: 17px;
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
 }
 
 .topnav a:hover {
-background-color: #ddd;
-color: black;
+  background-color: #ddd;
+  color: black;
 }
 
 .topnav a.active {
-background-color: #2196F3;
-color: white;
+  background-color: #2196F3;
+  color: white;
 }
 
 .topnav input[type=text] {
-float: right;
-padding: 6px;
-margin-top: 8px;
-margin-right: 16px;
-border: none;
-font-size: 17px;
+  float: right;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 16px;
+  border: none;
+  font-size: 17px;
 }
 
-.separateur{
+.separateur {
   width: 100%;
   height: 6%;
   background-color: black;
 }
 
-footer{
-  background-color:hsla(50, 33%, 25%, .75);
+.text {
+  text-indent: 50px;
+  text-align: center;
+  letter-spacing: 3px;
+  padding-left: 5%;
+  padding-right: 5%;
+  padding-top: 15%;
 }
 
+.fullscreen {
+  height: 100%;
+  width: 100%;
+
+}
+
+
+
+footer {
+  position: relative;
+  bottom: 0%;
+  background-color: hsla(50, 33%, 25%, .75);
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 5px;
+
+}
 </style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <div class="fullscreen">
+    <h1 class="text">
+      Membres: 
+    </h1>
+
+    <h1 class="about">
+      Paul DEVRED,
+    </h1>
+
+    <h1 class="about">
+     Alexy Vanot,
+    </h1>
+
+    <h1 class="about">
+    Robin PALMIER,
+    </h1>
+
+    <h1 class="about">
+    Pierre GERVAIS.
+    </h1>
+  </div>
+  
+</template>
